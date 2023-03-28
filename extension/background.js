29cab9e75767fbda96ef97725d7f03c25d90234a
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2023-03-28 18:35:14 -0400

    Make Chromium happy

--- extension/background.js
+++ extension/background.js
@@ -1,13 +1,28 @@
 "use strict";
-browser.webNavigation.onHistoryStateUpdated.addListener(function(event){
+
+/* not a great fix, but makes it work in Chromium */
+if(typeof browser === "undefined" && typeof chrome !== "undefined"){
+    var BUCE_browser = chrome;
+}
+else{
+    if(typeof browser !== "undefined"){
+        var BUCE_browser = browser;
+    }
+    else{
+        // we are somewhere with neither?
+        console.error("Neither browser nor chrome found!");
+    }
+}
+
+BUCE_browser.webNavigation.onHistoryStateUpdated.addListener(function(event){
     /*
     when history.replaceState is used by bilibili.com, check if vd_source is in use
     */
-   //console.log(event);
+   console.log(event);
    if(event.url.includes("vd_source")){ // this check is designed to avoid the overhead of constructing a URL object for all urls by limiting it to those probably with the vd_source param
     let tmpurlobject = new URL(event.url);
     if(tmpurlobject.searchParams.has("vd_source")){
-        browser.tabs.executeScript(event.tabId,{"file":"clean.js"});
+        BUCE_browser.tabs.executeScript(event.tabId,{"file":"clean.js"});
     }
    }
 },{"url":[{"hostContains":"bilibili.com"}]});
