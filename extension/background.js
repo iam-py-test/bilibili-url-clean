6dae7e70d52e9693cb2d15dc2cd8594683ce5d0a
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2023-03-28 18:37:35 -0400

    Don't spam the console unless I'm debugging it

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const DEBUG_ENABLED = false; // when testing
+
 /* not a great fix, but makes it work in Chromium */
 if(typeof browser === "undefined" && typeof chrome !== "undefined"){
     var BUCE_browser = chrome;
@@ -18,7 +20,9 @@ BUCE_browser.webNavigation.onHistoryStateUpdated.addListener(function(event){
     /*
     when history.replaceState is used by bilibili.com, check if vd_source is in use
     */
-   console.log(event);
+   if(DEBUG_ENABLED){
+    console.log("[BUCE] Got history.replaceState: ",event);
+   }
    if(event.url.includes("vd_source")){ // this check is designed to avoid the overhead of constructing a URL object for all urls by limiting it to those probably with the vd_source param
     let tmpurlobject = new URL(event.url);
     if(tmpurlobject.searchParams.has("vd_source")){
